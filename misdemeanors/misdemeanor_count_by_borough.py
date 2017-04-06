c74bd8614165d1f6f5c0de4d462ab0b70d88ac53
Zeynep Doganata <zdoganata@gmail.com>
2017-04-05 21:47:41 -0400

    counts of misdemeanors by boroughs across all years

---
+++ misdemeanors/misdemeanor_count_by_borough.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+import os
+from csv import reader
+
+#Count the total misdemeanor complaints by borough
+#output: key = borough, value = count
+
+
+if __name__ == "__main__":
+    def filter_date(x):
+        try:
+            x.split('/')[2]
+            return True
+        except Exception as e:
+            return False
+
+    def filter_misdemeanor(x):
+        if 'MISDEMEANOR' in x:
+            return True
+        else:
+            return False
+
+
+    if len(sys.argv) != 2:
+        print("Usage: misdemeanor_count_by_borough.py <file1> ", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    data= sc.textFile(sys.argv[1], 1)
+    rdd = data.mapPartitions(lambda x: reader(x))
+    mapping = rdd.map(lambda line: (line[5], line[11], line[13])).filter(lambda x: filter_date(x[0])).filter(lambda x: filter_misdemeanor(x[1])).map(lambda x: (x[2], 1))
+    joined = mapping.reduceByKey(lambda x,y: x+y).sortByKey(ascending=True).saveAsTextFile("misdemeanor_count_by_borough.out")
