d8f4ce49809285a01c21e09d5c4ebe5e58fbdfa8
Zeynep Doganata <zdoganata@gmail.com>
2017-04-10 19:31:00 -0400

    violations data and scripts

---
+++ violations/source/violation_count_by_hour.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+import os
+from csv import reader
+
+#Count the total violation complaints by hour of day 2006 - 2016
+#output: key = hour, value = count
+
+
+if __name__ == "__main__":
+    def filter_date(x):
+        try:
+            x.split('/')[0]
+            return True
+        except Exception as e:
+            return False
+
+    def filter_violation(x):
+        if 'VIOLATION' in x:
+            return True
+        else:
+            return False
+
+
+    if len(sys.argv) != 2:
+        print("Usage: violation_count_by_hour.py <file1> ", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    data= sc.textFile(sys.argv[1], 1)
+    rdd = data.mapPartitions(lambda x: reader(x))
+    mapping = rdd.map(lambda line: (line[2], line[11])).filter(lambda x: filter_violation(x[1])).map(lambda x: (x[0].split(':')[0], 1))
+    joined = mapping.reduceByKey(lambda x,y: x+y).sortByKey(ascending=True).saveAsTextFile("violation_count_by_hour.out")
---
+++ violations/source/violation_count_by_loc_and_prep.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+import os
+from csv import reader
+
+#Count the total violation complaints by the preposition and premise  describing where they were reported
+#output: key = preposition_premise, value = count
+
+
+if __name__ == "__main__":
+
+    def filter_violation(x):
+        if 'VIOLATION' in x:
+            return True
+        else:
+            return False
+
+
+    if len(sys.argv) != 2:
+        print("Usage: violation_count_by_loc_and_prep.py <file1> ", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    data= sc.textFile(sys.argv[1], 1)
+    rdd = data.mapPartitions(lambda x: reader(x))
+    mapping = rdd.map(lambda line: (line[11], line[15], line[16])).filter(lambda x: filter_violation(x[0])).map(lambda x: (str(x[1])+'_'+str(x[2]), 1))
+    joined = mapping.reduceByKey(lambda x,y: x+y).sortByKey(ascending=True).saveAsTextFile("violation_count_by_loc_and_prep.out")
